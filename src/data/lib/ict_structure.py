12680ef2a84ea0b830e26548a3ba8f924cf67e6c
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-29 04:55:30 +0000

    ${29AM12:55 AMe12:55 AMi55e}

---
+++ src/data/lib/ict_structure.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+
+def detect_swings():
+    pass
+
+def add_previous_swing_levels():
+    pass
+
+def detect_structure_shift():
+    pass
+
+def find_recent_external_liquidity():
+    pass
+
