36b157d0aee3ff7f1ce5a3a7808f79050efc4f71
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-29 04:57:20 +0000

    ${29AM12:57 AMe12:57 AMi57e}

--- src/data/lib/ict_liquidity.py
+++ src/data/lib/ict_liquidity.py
@@ -3,4 +3,9 @@ import pandas as pd
 from lib.ict_fvg import find_recent_fvg_context, price_touches_fvg
 from lib.ict_structure import find_recent_external_liquidity
 
-def add_liquidity_context()
+def add_liquidity_context():
+    pass
+
+def summarize_liquidity():
+    pass 
+
