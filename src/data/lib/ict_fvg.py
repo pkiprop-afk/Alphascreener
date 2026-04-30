602af79612f4f56133b2c4f51ae28251455d2c04
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-30 09:15:14 +0000

    ${30AM5:15 AMe5:15 AMi15e}

--- src/data/lib/ict_fvg.py
+++ src/data/lib/ict_fvg.py
@@ -1,7 +1,10 @@
 import pandas as pd 
 
-def detect_fvg_zones():
-    pass
+def detect_fvg_zones(price_df: pd.DataFrame, min_gap_pct: float = 0.001) -> pd.DataFrame:
+    df = price_df.copy()
+    df["bullish_fvg"] = False
+    df["bearish_fvg"] = False
+    df["fvg_top"] = pd.NA
 
 def find_recent_fvg_context():
     pass
