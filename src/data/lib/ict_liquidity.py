2db6aab03a715d9c9046eebd7defa1037c5529ad
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-30 10:22:40 +0000

    ${30AM6:22 AMe6:22 AMi22e}

--- src/data/lib/ict_liquidity.py
+++ src/data/lib/ict_liquidity.py
@@ -3,7 +3,7 @@ import pandas as pd
 from lib.ict_fvg import find_recent_fvg_context, price_touches_fvg
 from lib.ict_structure import find_recent_external_liquidity
 
-def add_liquidity_context():
+def add_liquidity_context(price_df: pd.DataFrame, liquidity_lookback: int = 40) -> pd.DataFrame:
     pass
 
 def summarize_liquidity():
