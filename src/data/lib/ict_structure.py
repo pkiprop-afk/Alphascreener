a9fa840fe18bc401b066cf35608b92decbfad8e6
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-30 11:02:49 +0000

    ${30AM7:02 AMe7:02 AMi2e}

--- src/data/lib/ict_structure.py
+++ src/data/lib/ict_structure.py
@@ -5,6 +5,9 @@ def detect_swings(price_df: pd.DataFrame, swing_window: int = 3) -> pd.DataFrame
     df["swing_high"] = False
     df["swing_low"] =False
     df["swing_high_value"] = pd.NA
+    
+    for idx in range(swing_window, len(df) - swing_window):
+        high_window = df.iloc[idx - swing_window : idx + swing_window + 1]["High"]
 
 def add_previous_swing_levels():
     pass
