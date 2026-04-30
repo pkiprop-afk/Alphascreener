767db3e6bf13d971ea071fc183a6d5a9b83c6d21
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-30 10:24:38 +0000

    ${30AM6:24 AMe6:24 AMi24e}

--- src/data/lib/ict_liquidity.py
+++ src/data/lib/ict_liquidity.py
@@ -4,7 +4,12 @@ from lib.ict_fvg import find_recent_fvg_context, price_touches_fvg
 from lib.ict_structure import find_recent_external_liquidity
 
 def add_liquidity_context(price_df: pd.DataFrame, liquidity_lookback: int = 40) -> pd.DataFrame:
-    pass
+    df = price_df.copy()
+    df["external_high"] = pd.NA
+    df["external_low"] = pd.NA
+    df["touches_internal_liquidity"] = False
+    df["touches_external_high"] = False
+    df["touches_external_low"] = False
 
 def summarize_liquidity():
     pass 
