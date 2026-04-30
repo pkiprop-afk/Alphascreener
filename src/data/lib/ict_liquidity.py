3ba7ce3a9c51769a2f8a053b9ec068ca26425dfc
Peter Kiprop <PKiprop@Caldwell.edu>
2026-04-30 10:34:35 +0000

    ${30AM6:34 AMe6:34 AMi34e}

--- src/data/lib/ict_liquidity.py
+++ src/data/lib/ict_liquidity.py
@@ -26,6 +26,8 @@ def add_liquidity_context(price_df: pd.DataFrame, liquidity_lookback: int = 40) 
             df.at[df.index[idx], "touches_external_high"] = candle_high >= float(external_high)
         if external_low is not None:
             df.at[df.index[idx], "touches_external_low"] = candle_low <= float(external_low)
+        
+        
 
 def summarize_liquidity():
     pass 
