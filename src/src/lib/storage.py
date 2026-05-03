13cb7781646715827e4d272a1269734ab626b318
Peter Kiprop <PKiprop@Caldwell.edu>
2026-05-03 06:43:28 +0000

    ${3AM2:43 AMe2:43 AMi43e}

--- src/src/lib/storage.py
+++ src/src/lib/storage.py
@@ -40,7 +40,11 @@ def timestamp_now() -> str:
     return datetime.now().isoformat(timespec="seconds")
 
 def load_json_file(file_path: str, default: Any):
-    pass
+    ensure_parents_exist(file_path)
+    if not os.path.exists(file_path):
+        with open(file_path, "w", encoding="utf-8") as file:
+            json.dump(default, file, indent=2)
+        return default
 
 def write_json_file():
     pass
