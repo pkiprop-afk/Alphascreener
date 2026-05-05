ea53f1e23f9f1f4c7069af328e1ef0b7c7cac97b
Peter Kiprop <PKiprop@Caldwell.edu>
2026-05-05 07:25:23 +0000

    ${5AM3:25 AMe3:25 AMi25e}

--- src/src/lib/market_data.py
+++ src/src/lib/market_data.py
@@ -143,7 +143,10 @@ def evaluate_bar_setup(signal_df: pd.DataFrame, index_position: int, strategy: d
         "structure_ok": structure_ok,
         "cisd_ok": cisd_ok,
         "fvg_ok": fvg_ok,
-        
+        "external_ok": external_ok,
+        "internal_ok": internal_ok,
+        "pattern_ok": pattern_ok,
+        "setup_valid": setup_valid,
     }
 def build_summary_row():
     pass
